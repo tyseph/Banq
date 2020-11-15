0df3d46450bea60dc28181873d883ea1d68bcff0
Sparsh <sparsh.ailawadi@gmail.com>
2020-11-15 22:28:05 +0530

    test101

--- components/Profile.js
+++ components/Profile.js
@@ -1,24 +1,23 @@
-import React, { Component } from 'react';
-import { Text, View, Button } from 'react-native';
+import React, {Component} from 'react';
+import {Text, View, Button} from 'react-native';
 
 import auth from '@react-native-firebase/auth';
 
 export default class Profile extends Component {
+  // SignOUt = () => {
+  //   auth()
+  //   .signOut()
+  //   .then(() =>{
+  //     window.alert('Signed Out');
+  //   })
+  // }
 
-  SignOUt = () => {
-    auth()
-    .signOut()
-    .then(() =>{
-      window.alert('Signed Out');
-    })
+  render() {
+    return (
+      <View>
+        <Text>Profile!</Text>
+        <Button title="Logout" onPress={this.SignOUt} />
+      </View>
+    );
   }
-
-  render(){
-  return (
-    <View>
-      <Text>Profile!</Text>
-      <Button title="Logout" onPress={ this.SignOUt }/>
-    </View>
-  );
-}
 }
