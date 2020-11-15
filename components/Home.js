0f5ea3abbf5d6d1b54bb832bcfd6e877537559dd
Sparsh <sparsh.ailawadi@gmail.com>
2020-11-16 01:03:39 +0530

    home style

--- components/Home.js
+++ components/Home.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-// import {View, Button, FlatList} from 'react-native';
+import {ImageBackground, Image} from 'react-native';
 import {
   Container,
   Header,
@@ -8,6 +8,14 @@ import {
   CardItem,
   Body,
   Text,
+  Grid,
+  Col,
+  Row,
+  Left,
+  Thumbnail,
+  Button,
+  Icon,
+  Right,
 } from 'native-base';
 
 //import NavBar from '../elements/NavBar';
@@ -62,15 +70,66 @@ export default class HomeScreen extends Component {
     // if (this.state.openNavBar == false) {
     return (
       <Container>
-        <Header />
         <Content>
-          <Card>
-            <CardItem>
-              <Body>
-                <Text>THIS IS A CARD</Text>
-              </Body>
-            </CardItem>
-          </Card>
+          <Grid>
+            <Row
+              style={{
+                height: 400,
+                borderBottomLeftRadius: 20,
+                borderBottomRightRadius: 20,
+              }}>
+              <ImageBackground
+                style={{
+                  flex: 1,
+                  flexDirection: 'row',
+                  width: '100%',
+                  height: '100%',
+                  justifyContent: 'flex-start',
+                  // alignItems: 'center',
+                  opacity: 1,
+                }}
+                imageStyle={{
+                  borderBottomLeftRadius: 25,
+                  borderBottomRightRadius: 25,
+                }}
+                source={require('../assets/bg.png')}>
+                <Image
+                  source={require('../assets/logo.png')}
+                  style={{
+                    width: 40,
+                    height: 51,
+                    resizeMode: 'contain',
+                    justifyContent: 'flex-start',
+                    padding: 25,
+                    margin: 10,
+                  }}
+                />
+              </ImageBackground>
+            </Row>
+
+            <Row style={{backgroundColor: 'black', height: 300}}>
+              <Card transparent>
+                <CardItem transparent>
+                  <Thumbnail source={require('../assets/thumbnail.png')} />
+                  <Body>
+                    <Text
+                      style={{
+                        color: 'white',
+                      }}>
+                      NativeBase
+                    </Text>
+                    <Text
+                      note
+                      style={{
+                        color: 'black',
+                      }}>
+                      > GeekyAnts
+                    </Text>
+                  </Body>
+                </CardItem>
+              </Card>
+            </Row>
+          </Grid>
         </Content>
       </Container>
     );
