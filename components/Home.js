ebdccea94f3d7c89783900e7493fac047d23c03b
Sparsh <sparsh.ailawadi@gmail.com>
2020-11-16 02:23:37 +0530

    commit2

--- components/Home.js
+++ components/Home.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {ImageBackground, Image} from 'react-native';
+import {ImageBackground, StyleSheet, View, Image} from 'react-native';
 import {
   Container,
   Header,
@@ -69,69 +69,146 @@ export default class HomeScreen extends Component {
   render() {
     // if (this.state.openNavBar == false) {
     return (
-      <Container>
-        <Content>
-          <Grid>
-            <Row
-              style={{
-                height: 400,
-                borderBottomLeftRadius: 20,
-                borderBottomRightRadius: 20,
-              }}>
-              <ImageBackground
-                style={{
-                  flex: 1,
-                  flexDirection: 'row',
-                  width: '100%',
-                  height: '100%',
-                  justifyContent: 'flex-start',
-                  // alignItems: 'center',
-                  opacity: 1,
-                }}
-                imageStyle={{
-                  borderBottomLeftRadius: 25,
-                  borderBottomRightRadius: 25,
-                }}
-                source={require('../assets/bg.png')}>
-                <Image
-                  source={require('../assets/logo.png')}
-                  style={{
-                    width: 40,
-                    height: 51,
-                    resizeMode: 'contain',
-                    justifyContent: 'flex-start',
-                    padding: 25,
-                    margin: 10,
-                  }}
-                />
-              </ImageBackground>
-            </Row>
+      <View style={{backgroundColor: 'white'}}>
+        <ImageBackground
+          style={styles.imgBackground}
+          imageStyle={styles.imgBG}
+          source={require('../assets/bg.png')}>
+          <Image
+            source={require('../assets/logo.png')}
+            style={styles.logoStyle}
+          />
+          <Card transparent style={styles.nameCard}>
+            <CardItem style={styles.cardItemStyle}>
+              <Left>
+                <Thumbnail large source={require('../assets/thumbnail.png')} />
+                <Body>
+                  <Text note style={{color: 'white'}}>
+                    Welcome Back
+                  </Text>
+                  <Text style={{color: 'white'}}>Sajal Ganjewala</Text>
+                </Body>
+              </Left>
+            </CardItem>
+          </Card>
 
-            <Row style={{backgroundColor: 'black', height: 300}}>
-              <Card transparent>
-                <CardItem transparent>
-                  <Thumbnail source={require('../assets/thumbnail.png')} />
-                  <Body>
-                    <Text
-                      style={{
-                        color: 'white',
-                      }}>
-                      NativeBase
-                    </Text>
-                    <Text
-                      note
-                      style={{
-                        color: 'black',
-                      }}>
-                      ekyAnts
-                    </Text>
-                  </Body>
-                </CardItem>
-              </Card>
-            </Row>
-          </Grid>
-        </Content>
-      </Container>
+          <Card
+            style={{
+              alignSelf: 'center',
+              padding: 20,
+              height: 100,
+              width: 350,
+              borderRadius: 20,
+            }}>
+            <Grid>
+              <Col>
+                <Body
+                  style={{
+                    textAlign: 'left',
+                    padding: 0,
+                    height: 100,
+                    width: 300,
+                    borderRadius: 20,
+                  }}>
+                  <Text style={styles.largeText}>BANQ</Text>
+                  <Text note style={styles.smallText}>
+                    Budget
+                  </Text>
+                </Body>
+              </Col>
+              <Col>
+                <Body>
+                  <Text style={styles.rightText}>INR 5673</Text>
+                </Body>
+              </Col>
+            </Grid>
+          </Card>
+
+          <Card
+            style={{
+              alignSelf: 'center',
+              padding: 20,
+              height: 150,
+              width: 350,
+              borderRadius: 20,
+            }}>
+            <Grid>
+              <Col>
+                <Body
+                  style={{
+                    textAlign: 'left',
+                    padding: 0,
+                    height: 100,
+                    width: 300,
+                    borderRadius: 20,
+                  }}>
+                  <Text style={styles.largeText}>Total</Text>
+                  <Text note style={styles.smallText}>
+                    Cash Available
+                  </Text>
+                </Body>
+              </Col>
+              <Col>
+                <Body>
+                  <Text style={styles.rightText}>INR 5637</Text>
+                </Body>
+              </Col>
+            </Grid>
+            <Grid style={{marginTop: 0}}>
+              <Col>
+                <Body
+                  style={{
+                    textAlign: 'left',
+                    padding: 0,
+                    height: 100,
+                    width: 300,
+                    borderRadius: 20,
+                  }}>
+                  <Text style={styles.smallText}>Days Left</Text>
+                  <Text note style={styles.largeText}>
+                    Days in Month
+                  </Text>
+                </Body>
+              </Col>
+              <Col>
+                <Body>
+                  <Text style={styles.rightText}>INR 5673</Text>
+                </Body>
+              </Col>
+            </Grid>
+          </Card>
+
+          {/* pie chart */}
+          <Card
+            style={{
+              alignSelf: 'center',
+              padding: 20,
+              height: 150,
+              width: 350,
+              borderRadius: 20,
+            }}>
+            <Grid>
+              <Col size={2} style={{backgroundColor: 'red', borderRadius: 20}}>
+                <Body
+                  style={{
+                    textAlign: 'left',
+                    padding: 0,
+                    height: 100,
+                    width: 300,
+                    borderRadius: 20,
+                  }}>
+                  <Text style={styles.largeText}>LEGEND</Text>
+                </Body>
+              </Col>
+              <Col size={3}>
+                <Body>
+                  <Text style={styles.rightText}>PIE CHART HERE</Text>
+                </Body>
+              </Col>
+            </Grid>
+          </Card>
+        </ImageBackground>
+      </View>
     );
     // } else {
     //   return (
@@ -143,3 +220,33 @@ export default class HomeScreen extends Component {
     // }
   }
 }
+
+const styles = StyleSheet.create({
+  imgBackground: {
+    width: '100%',
+    height: '80%',
+    resizeMode: 'contain',
+  },
+  imgBG: {
+    borderBottomLeftRadius: 25,
+    borderBottomRightRadius: 25,
+  },
+  logoStyle: {
+    width: 40,
+    height: 51,
+    resizeMode: 'contain',
+    justifyContent: 'flex-start',
+    padding: 25,
+    margin: 10,
+  },
+  nameCard: {textAlign: 'center', padding: 20},
+  cardItemStyle: {backgroundColor: 'transparent'},
+  largeText: {
+    color: 'black',
+    fontFamily: 'Oxygen-Bold',
+  },
+  smallText: {
+    color: 'black',
+  },
+  rightText: {color: 'black'},
+});
