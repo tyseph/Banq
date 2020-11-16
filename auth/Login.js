8fa95c3fa33624ae9a42b60dc3a405f8f9b8d536
tyseph <sg748@snu.edu.in>
2020-11-16 18:42:14 +0530

    commit-16th Nov

--- auth/Login.js
+++ auth/Login.js
@@ -1,91 +1,170 @@
-import React, { Component } from 'react';
-import { Button, View, Text, ImageBackground, Image, TextInput } from 'react-native';
+import React, {Component} from 'react';
+import {
+  View,
+  Text,
+  ImageBackground,
+  Image,
+  TextInput,
+  StyleSheet,
+} from 'react-native';
+import { Button,
+        CardItem, 
+        Body} from 'native-base';
 
 export default class Login extends Component {
-        //   constructor(props) {
-        //     super(props);
-        //     this.state = {
-        //       email: '',
-        //       password: '',
-        //     };
-        //   }
+//     constructor(props) {
+//        super(props);
+//        this.state = {
+//          email: '',
+//          password: '',
+//        };
+//      }
 
-        //   SignIn = () => {
-        //     let email = this.state.email;
-        //     let password = this.state.password;
+  //   SignIn = () => {
+  //     let email = this.state.email;
+  //     let password = this.state.password;
 
-        //     console.log(email + ' - ' + password + ' - ' + displayName);
+  //     console.log(email + ' - ' + password + ' - ' + displayName);
 
-        //     if (!email || !password) {
-        //       window.alert('Enter Username and Password');
-        //     } else {
-        //       auth()
-        //         .signInWithEmailAndPassword(email, password)
-        //         .then(() => {
-        //           console.log('signed in!');
-        //           // this.props.navigation.navigate('Home');
-        //         })
-        //         .catch(error => {
-        //           if (error.code === 'auth/email-already-in-use') {
-        //             console.log('That email address is already in use!');
-        //           }
+  //     if (!email || !password) {
+  //       window.alert('Enter Username and Password');
+  //     } else {
+  //       auth()
+  //         .signInWithEmailAndPassword(email, password)
+  //         .then(() => {
+  //           console.log('signed in!');
+  //           // this.props.navigation.navigate('Home');
+  //         })
+  //         .catch(error => {
+  //           if (error.code === 'auth/email-already-in-use') {
+  //             console.log('That email address is already in use!');
+  //           }
 
-        //           if (error.code === 'auth/invalid-email') {
-        //             console.log('That email address is invalid!');
-        //           }
+  //           if (error.code === 'auth/invalid-email') {
+  //             console.log('That email address is invalid!');
+  //           }
 
-        //           console.error(error);
-        //         });
-        //     }
-        //   };
+  //           console.error(error);
+  //         });
+  //     }
+  //   };
 
-        render() {
-                return (
-                        <View>
-                                <ImageBackground
-                                        style={{
-                                                width: '100%',
-                                                height: '80%',
-                                                resizeMode: 'contain'
+  render() {
+    return (
+      <>
+        <ImageBackground
+          style={{
+            width: '100%',
+            height: '72%',
+            resizeMode: 'contain',
+          }}
+          imageStyle={{
+            borderBottomLeftRadius: 25,
+            borderBottomRightRadius: 25,
+          }}
+          source={require('../assets/bg.png')}>
+          <Image
+            source={require('../assets/logo.png')}
+            style={{
+              width: 40,
+              height: 51,
+              resizeMode: 'contain',
+              justifyContent: 'flex-start',
+              padding: 25,
+              margin: 10,
+            }}
+          />
 
-                                        }}
-                                        imageStyle={{
-                                                borderBottomLeftRadius: 25,
-                                                borderBottomRightRadius: 25,
-                                        }}
-                                        source={require('../assets/bg.png')}>
-                                        <Image
-                                                source={require('../assets/logo.png')}
-                                                style={{
-                                                        width: 40,
-                                                        height: 51,
-                                                        resizeMode: 'contain',
-                                                        justifyContent: 'flex-start',
-                                                        padding: 25,
-                                                        margin: 10,
-                                                }}
-                                        />
-                                <Text>Login</Text>
-                                <TextInput
-                                        placeholder="Username"
-                                />
-                                <TextInput
-                                        placeholder="Password"
-                                />
-                                <Button
-                                        title="Login"
-                                        onPress={() => {
-                                                this.props.navigation.navigate('Home');
-                                        }}
-                                />
-                                <Button
-                                        title="Go to register"
-                                        onPress={() => {
-                                                this.props.navigation.navigate('Register');
-                                        }}
-                                />
-                                </ImageBackground>
-                        </View>
-                );
-        }
+          <CardItem style={styles.logocard}>
+            <Image
+              source={require('../assets/logo.png')}
+              style={{
+                width: 83,
+                height: 108,
+                resizeMode: 'contain',
+                margin: 10,
+              }}
+            />
+          </CardItem>
+
+          <CardItem style={styles.cardItem}>
+            <Body style={styles.userBody}>
+              <View style={styles.viewStyle}>
+                <TextInput
+                  style={styles.textInputStyle}
+                  placeholder="Username"
+                />
+              </View>
+
+              <View style={styles.viewStyle}>
+                <TextInput
+                  style={styles.textInputStyle}
+                  placeholder="Password"
+                />
+              </View>
+
+              <Button full
+                onPress={() => {this.props.navigation.navigate('Home')}}
+                style={styles.buttonStyle}>
+                <Text style={styles.buttonTextStyle}>Login</Text>
+              </Button>
+            </Body>
+          </CardItem>
+          <Text style={styles.linkTextStyle}>Don't have an account? <Text style={styles.linkStyle} onPress={() => this.props.navigation.navigate('Register')}>Register Here</Text></Text>
+        </ImageBackground>
+      </>
+    );
+  }
 }
+
+const styles = StyleSheet.create({
+  textInputStyle: {
+    fontSize: 20,
+    fontFamily: 'Oxygen-Regular',
+    fontWeight: '600',
+    marginLeft: -10,
+    paddingBottom: 3,
+  },
+  viewStyle: {
+    borderBottomWidth: 1,
+    width: '100%',
+    borderBottomColor: 'gray',
+    marginBottom: 25,
+  },
+  cardItem: {
+    borderRadius: 25,
+    marginTop: 20,
+    alignSelf: 'center',
+    width: '90%',
+  },
+  logocard: {
+    borderRadius: 500,
+    width: 200,
+    height: 200,
+    alignSelf: 'center',
+    justifyContent: 'center',
+    marginTop: 20,
+  },
+  userBody: {
+    padding: 25,
+  },
+  buttonStyle: {
+    borderRadius: 25,
+
+    backgroundColor: '#096348',
+  },
+  buttonTextStyle: {
+    fontSize: 30,
+    fontFamily: 'Oxygen-Bold',
+    fontWeight: 'bold',
+    color: 'white',
+  },
+  linkTextStyle: {
+          alignSelf: 'center',
+          marginTop : 10,
+          fontFamily: 'Oxygen-Light',
+  },
+  linkStyle: {
+          color: 'blue',
+  }
+});
--- auth/Register.js
+++ auth/Register.js
@@ -1,69 +1,190 @@
-import React, { Component } from 'react';
-import { email, Button, View, Text, TextTextInput } from 'react-native';
-import auth from '@react-native-firebase/auth';
-import { TextInput } from 'react-native-gesture-handler';
+import React, {Component} from 'react';
+import {
+  View,
+  Text,
+  ImageBackground,
+  Image,
+  TextInput,
+  StyleSheet,
+} from 'react-native';
+import {Button, CardItem, Body} from 'native-base';
 
-export default class Login extends Component{
-        
-        constructor(props) {
-                super(props);
-                this.state = {
-                        displayName: '',
-                        email: '',
-                        password: '',
-                        confirmPassword: ''
-                };
-              }
+export default class Login extends Component {
+           constructor(props) {
+                   super(props);
+                   this.state = {
+                           displayName: '',
+                           email: '',
+                           password: '',
+                           confirmPassword: ''
+                   };
+                 }
 
-        Register = () => {
+  //         Register = () => {
 
-                let displayName = this.state.displayName;
-                let email = this.state.email;
-                let password = this.state.password;
-                let checkPassword = this.state.checkPassword;
+  //                 let displayName = this.state.displayName;
+  //                 let email = this.state.email;
+  //                 let password = this.state.password;
+  //                 let checkPassword = this.state.checkPassword;
 
-                console.log(displayName+ " " + email + " " + password + "" + checkPassword);
-                
-                if (!email || !password) {
-                        window.alert('Enter Username and Password');
-                }
+  //                 console.log(displayName+ " " + email + " " + password + "" + checkPassword);
 
-                else if (password !== checkPassword ) {
-                        window.alert('Incorrect Password');
-                }
+  //                 if (!email || !password) {
+  //                         window.alert('Enter Username and Password');
+  //                 }
 
-                else { 
-                        auth()
-                        .createUserWithEmailAndPassword(email, password)
-                        .then(() => {
-                        console.log('signed in!');
-                        // this.props.navigation.navigate('Home');
-                        })
-                        .catch(error => {
-                        if (error.code === 'auth/email-already-in-use') {
-                        window.alert('That email address is already in use!');
-                        }
+  //                 else if (password !== checkPassword ) {
+  //                         window.alert('Incorrect Password');
+  //                 }
 
-                        if (error.code === 'auth/invalid-email') {
-                        window.alert('That email address is invalid!');
-                        }
+  //                 else {
+  //                         auth()
+  //                         .createUserWithEmailAndPassword(email, password)
+  //                         .then(() => {
+  //                         console.log('signed in!');
+  //                         // this.props.navigation.navigate('Home');
+  //                         })
+  //                         .catch(error => {
+  //                         if (error.code === 'auth/email-already-in-use') {
+  //                         window.alert('That email address is already in use!');
+  //                         }
 
-                        console.error(error);
-                        });
-        }
+  //                         if (error.code === 'auth/invalid-email') {
+  //                         window.alert('That email address is invalid!');
+  //                         }
+
+  //                         console.error(error);
+  //                         });
+  //         }
+  // }
+
+  render() {
+    return (
+      <>
+        <ImageBackground
+          style={{
+            width: '100%',
+            height: '72%'
+          }}
+          imageStyle={{
+            borderBottomLeftRadius: 25,
+            borderBottomRightRadius: 25,
+          }}
+          source={require('../assets/bg.png')}>
+          <Image
+            source={require('../assets/logo.png')}
+            style={{
+              width: 40,
+              height: 51,
+              resizeMode: 'contain',
+              justifyContent: 'flex-start',
+              padding: 25,
+              margin: 10,
+            }}
+          />
+
+          <CardItem style={styles.logocard}>
+            <Image
+              source={require('../assets/logo.png')}
+              style={{
+                width: 83,
+                height: 108,
+                resizeMode: 'contain',
+                margin: 10,
+              }}
+            />
+          </CardItem>
+
+          <CardItem style={styles.cardItem}>
+            <Body style={styles.userBody}>
+              <View style={styles.viewStyle}>
+                <TextInput style={styles.textInputStyle} placeholder="Name" />
+              </View>
+
+              <View style={styles.viewStyle}>
+                <TextInput style={styles.textInputStyle} placeholder="Email" />
+              </View>
+
+              <View style={styles.viewStyle}>
+                <TextInput
+                  style={styles.textInputStyle}
+                  placeholder="Password"
+                />
+              </View>
+
+              <View style={styles.viewStyle}>
+                <TextInput
+                  style={styles.textInputStyle}
+                  placeholder="Confirm Password"
+                />
+              </View>
+
+              <Button full onPress={this.register} style={styles.buttonStyle}>
+                <Text style={styles.buttonTextStyle}>Register</Text>
+              </Button>
+            </Body>
+          </CardItem>
+          <Text style={styles.linkTextStyle}>
+            Already have an account? <Text
+              style={styles.linkStyle}
+              onPress={() => this.props.navigation.navigate('Login')}>
+              Login Here
+            </Text>
+          </Text>
+        </ImageBackground>
+      </>
+    );
+  }
 }
-        
-        render(){
-            return(
-                <View>
-                <Text>Register</Text>
-                <TextInput placeholder="Name" onChangeText={(displayName) => this.setState({displayName})} value={this.state.displayName} />
-                <TextInput placeholder="Username" onChangeText={(email) => this.setState({email})} value={this.state.email} />
-                <TextInput placeholder="Password" onChangeText={(password) => this.setState({password})} value={this.state.password} />
-                <TextInput placeholder="Confirm Password" onChangeText={(checkPassword) => this.setState({checkPassword})} value={this.state.checkPassword} />
-                <Button title="Register" onPress={this.Register}/>
-                <Button title="Go to login" onPress={()=>{this.props.navigation.navigate('Login')}}/>
-          </View>
-            )
-        }
-      }
+
+const styles = StyleSheet.create({
+  textInputStyle: {
+    fontSize: 20,
+    fontFamily: 'Oxygen-Regular',
+    fontWeight: '600',
+    marginLeft: -10,
+    paddingBottom: 3,
+  },
+  viewStyle: {
+    borderBottomWidth: 1,
+    width: '100%',
+    borderBottomColor: 'gray',
+    marginBottom: 25,
+  },
+  cardItem: {
+    borderRadius: 25,
+    marginTop: 20,
+    alignSelf: 'center',
+    width: '90%',
+  },
+  logocard: {
+    borderRadius: 500,
+    width: 200,
+    height: 200,
+    alignSelf: 'center',
+    justifyContent: 'center',
+    marginTop: 20,
+  },
+  userBody: {
+    padding: 25,
+  },
+  buttonStyle: {
+    borderRadius: 25,
+
+    backgroundColor: '#096348',
+  },
+  buttonTextStyle: {
+    fontSize: 30,
+    fontFamily: 'Oxygen-Bold',
+    fontWeight: 'bold',
+    color: 'white',
+  },
+  linkTextStyle: {
+    alignSelf: 'center',
+    marginTop: 10,
+    fontFamily: 'Oxygen-Light',
+  },
+  linkStyle: {
+    color: 'blue',
+  },
+});
